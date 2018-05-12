7d0bcaaa4292f4b36468f994d8130b71d9ef7951
Markus Felten <Markus.Felten@gmx.de>
2018-05-12 06:52:37 +0200

    feat: parse date

--- src/log-stream-analyser.mjs
+++ src/log-stream-analyser.mjs
@@ -16,62 +16,52 @@ class LogStreamAnalyser {
       const i = stream[Symbol.asyncIterator]();
       const r = await i.next();
       if (r.done === false) {
-        yield r.value;
+        const lines = r.value.split(/\r?\n/);
+        for (const line of lines) {
+          const m = line.match(
+            /^(\w+)\s+(\d+)\s+(\d+):(\d+):(\d+)\s+(\w+)\s+(.*)/
+          );
+          if (m) {
+            const m2n = {
+              Jan: 0,
+              Feb: 1,
+              Mar: 2,
+              Apr: 3,
+              May: 4,
+              Jun: 5,
+              Jul: 6,
+              Aug: 7,
+              Sep: 8,
+              Oct: 9,
+              Nov: 10,
+              Dec: 11
+            };
+            yield {
+              date: new Date(2018, m2n[m[1]], m[2], m[3], m[4], m[5]),
+              host: m[6],
+              message: m[7]
+            };
+          }
+        }
       }
     }
   }
-
-  /*
-  async *[Symbol.asyncIterator]() {
-    const sources = this.sources;
-    return {
-      async next() {
-        for (const stream of sources) {
-          const i = stream[Symbol.asyncIterator]();
-          const r = await i.next();
-          if (r.done === false) {
-            //const lines = r.value.split(/\r?\n/);
-
-            return { done: false, value: r.value };
-          }
-        }
-
-        return { done: true };
-      }
-    };
-  }
-*/
 
   addSource(stream) {
     stream.setEncoding('utf8');
 
     this.sources.push(stream);
-
-    //console.log(stream);
-    //console.log(stream[Symbol.asyncIterator]);
-
-    /*
-    const i = stream[Symbol.asyncIterator]();
-    const r = await i.next();
-    console.log(r.value);
-*/
-    /*
-    for await (const chunk of stream) {
-      console.log(chunk);
-    }
-    */
   }
 }
 
 async function ab() {
   const lsa = new LogStreamAnalyser();
-  //lsa.addSource(process.stdin);
   lsa.addSource(
     createReadStream(join(__dirname, '..', 'tests', 'fixtures', 'install.log'))
   );
 
   for await (const e of lsa) {
-    console.log(e);
+    console.log(`event:${JSON.stringify(e)}`);
   }
 }
 
