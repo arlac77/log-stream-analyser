0c1e6a4c7443ed7b5f085bf25df096322399ff8a
Markus Felten <Markus.Felten@gmx.de>
2018-06-15 23:11:33 +0200

    chore: try to use *.mjs for ava tests too

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -3,9 +3,9 @@ import commonjs from 'rollup-plugin-commonjs';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  input: 'tests/**/*-test.js',
+  input: 'tests/**/*-test.mjs',
   output: {
-    file: 'build/bundle-test.js',
+    file: 'build/bundle-test.mjs',
     format: 'cjs',
     sourcemap: true
   },
