a0d9f9f2719a4fe08c3053dba73708b805525de4
Markus Felten <Markus.Felten@gmx.de>
2018-07-28 00:39:55 +0200

    chore(rollup): interop: false

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import executable from 'rollup-plugin-executable';
-
 import pkg from './package.json';
 
 export default [
@@ -11,7 +10,8 @@ export default [
       output: {
         file: pkg.bin[name],
         format: 'cjs',
-        banner: '#!/usr/bin/env node'
+        banner: '#!/usr/bin/env node',
+        interop: false
       },
       plugins: [resolve(), commonjs(), executable()]
     };
@@ -20,7 +20,8 @@ export default [
     input: pkg.module,
     output: {
       file: pkg.main,
-      format: 'cjs'
+      format: 'cjs',
+      interop: false
     },
     plugins: [resolve(), commonjs()]
   }
