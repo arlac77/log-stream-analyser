a1aa69b3f03d372646400d301ebe21411d63168d
Markus Felten <Markus.Felten@gmx.de>
2018-09-01 00:52:16 +0200

    chore: use @babel/plugin-proposal-async-generator-functions

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,34 @@
-import cleanup from 'rollup-plugin-cleanup';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import pkg from "./package.json";
+import babel from "rollup-plugin-babel";
+
+const plugins = [
+  resolve(),
+  commonjs(),
+  babel({
+    runtimeHelpers: false,
+    externalHelpers: true,
+    babelrc: false,
+    plugins: ["@babel/plugin-proposal-async-generator-functions"],
+    presets: [
+      [
+        "env",
+        {
+          targets: {
+            node: "10"
+          },
+          modules: false,
+          loose: true
+        }
+      ]
+    ],
+    exclude: "node_modules/**"
+  }),
+  cleanup()
+];
 
 export default [
   ...Object.keys(pkg.bin || {}).map(name => {
@@ -10,20 +36,20 @@ export default [
       input: `src/${name}.js`,
       output: {
         file: pkg.bin[name],
-        format: 'cjs',
-        banner: '#!/usr/bin/env node',
+        format: "cjs",
+        banner: "#!/usr/bin/env node",
         interop: false
       },
-      plugins: [resolve(), commonjs(), executable()]
+      plugins: [...plugins, executable()]
     };
   }),
   {
     input: pkg.module,
     output: {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       interop: false
     },
-    plugins: [resolve(), commonjs()]
+    plugins
   }
 ];
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,23 +1,43 @@
-import istanbul from 'rollup-plugin-istanbul';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import multiEntry from 'rollup-plugin-multi-entry';
+import istanbul from "rollup-plugin-istanbul";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import multiEntry from "rollup-plugin-multi-entry";
+import babel from "rollup-plugin-babel";
 
 export default {
-  input: 'tests/**/*-test.mjs',
+  input: "tests/**/*-test.mjs",
   output: {
-    file: 'build/bundle-test.mjs',
-    format: 'cjs',
+    file: "build/bundle-test.mjs",
+    format: "cjs",
     sourcemap: true,
     interop: false
   },
-  external: ['ava', 'fs', 'path'],
+  external: ["ava", "fs", "path"],
   plugins: [
+    babel({
+      runtimeHelpers: false,
+      externalHelpers: true,
+      babelrc: false,
+      plugins: ["@babel/plugin-proposal-async-generator-functions"],
+      presets: [
+        [
+          "env",
+          {
+            targets: {
+              node: "10"
+            },
+            modules: false,
+            loose: true
+          }
+        ]
+      ],
+      exclude: "node_modules/**"
+    }),
     multiEntry(),
     resolve(),
     commonjs(),
     istanbul({
-      exclude: ['tests/**/*-test.js']
+      exclude: ["tests/**/*-test.mjs", "node_modules/**/*"]
     })
   ]
 };
