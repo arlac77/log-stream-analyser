2980f2406735c23a4ad50c8d48c1ba2ef3d616d4
Markus Felten <Markus.Felten@gmx.de>
2018-09-09 00:48:02 +0200

    test: wip

--- tests/simple-test.mjs
+++ tests/simple-test.mjs
@@ -1,14 +1,15 @@
-import test from 'ava';
-import { join } from 'path';
-import { createReadStream } from 'fs';
-import { LogStreamAnalyser } from '../src/log-stream-analyser';
+import test from "ava";
+import { join } from "path";
+import { createReadStream } from "fs";
+import { LogStreamAggregator } from "../src/log-stream-aggregator";
+import { LogStreamAnalyser } from "../src/log-stream-analyser";
 
-test('install.log', async t => {
-  const lsa = new LogStreamAnalyser();
+test("install.log", async t => {
+  const lsa = new LogStreamAggregator();
 
   lsa.addSource(
     createReadStream(
-      join(__dirname, '..', 'tests', 'fixtures', 'install.log.txt')
+      join(__dirname, "..", "tests", "fixtures", "install.log.txt")
     )
   );
 
@@ -18,14 +19,16 @@ test('install.log', async t => {
   }
   */
 
-  const eventIter = await lsa[Symbol.asyncIterator]();
+  const analyser = new LogStreamAnalyser();
+
+  const eventIter = await analyser[Symbol.asyncIterator]();
 
   const event = (await eventIter.next()).value;
 
   t.deepEqual(event, {
-    date: 'Sep 24 22:04:14',
-    hostname: 'pro',
+    date: "Sep 24 22:04:14",
+    hostname: "pro",
     message:
-      'softwareupdate_firstrun_tasks[63]: Host swscan.apple.com isReachable = YES'
+      "softwareupdate_firstrun_tasks[63]: Host swscan.apple.com isReachable = YES"
   });
 });
